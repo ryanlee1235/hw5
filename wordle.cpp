08f53143152378ad8e8ac0239b9192762f03ba25
Ryan Lee <ryan.hyunjun.lee@gmail.com>
2025-04-27 17:43:27 -0700

    initial working wordle; passes driver tests

--- wordle.cpp
+++ wordle.cpp
@@ -13,7 +13,9 @@ using namespace std;
 
 
 // Add prototypes of helper functions here
-
+void wordleHelper(const std::string& in,
+    const std::string& floating,
+    const std::set<std::string>& dict, int index, std::set<std::string>& generated);
 
 // Definition of primary wordle function
 std::set<std::string> wordle(
@@ -22,7 +24,59 @@ std::set<std::string> wordle(
     const std::set<std::string>& dict)
 {
     // Add your code here
-
+    std::set<std::string> generated;
+    wordleHelper(in, floating, dict, 0, generated);
+    return generated;
 }
 
 // Define any helper functions here
+void wordleHelper(const std::string& in,
+    const std::string& floating,
+    const std::set<std::string>& dict, int index, std::set<std::string>& generated)
+{
+    if(index == in.size())
+    {
+        if(floating.empty())
+        {
+            if(dict.find(in) != dict.end())
+            {
+                generated.insert(in); // if word exists in dictionary, insert into the set i created
+            }
+        }
+        return;
+    }
+    if(in[index] != '-')
+    {
+        wordleHelper(in, floating, dict, index + 1, generated); // just skip the index if this is a fixed value
+    }
+    else
+    {
+        for(int k = 0; k < floating.size(); k++)
+        {
+            std::string word = in;
+            word[index] = floating[k];
+            string newFloating = floating.substr(0, k) + floating.substr(k + 1, floating.size()); // remove the current floating letter
+            wordleHelper(word, newFloating, dict, index + 1, generated);
+        }
+        for(int i = 0; i < 26; i++)
+        {
+            std::string word = in;
+            word[index] = 'a' + i; // loop through every character in alpha and then recurse w each option in the blank
+            wordleHelper(word, floating, dict, index + 1, generated);
+        }
+    }
+    return;
+}
+/* initial logic: start out with each floating letter in each available spot. In the empty spots cycle through all possible letters
+cycle through different combinations of floating letters (only need to do one of each because if it is repeated it will be covered)
+eg: if floating letters are n and d and the fixed letters are -i-- then you can do nid_ (cycle through everything) then din_ (cycle
+through everything) and then ni_d and then di_n)
+
+recursion parameters:
+try first parameter and lock it in
+insert new string
+try first parameter and lock it in
+cycle through all 
+
+for however many blanks that there aren't floating
+*/
